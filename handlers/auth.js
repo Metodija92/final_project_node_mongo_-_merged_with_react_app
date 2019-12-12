06e8c8d79de191898301af75cc7493e86c641d07
Meto <licovskim@gmail.com>
2019-12-12 12:26:05 +0100

    Update auth.js

--- handlers/auth.js
+++ handlers/auth.js
@@ -51,7 +51,7 @@ const login = (req, res) => {
                     email: data.email
                 };
                 var token = jwt.sign(tokenData, config.getConfig('jwt').key);
-                return res.status(200).send({jwt: token});
+                return res.status(200).send({jwt: token, first_name: data.first_name, last_name: data.last_name});
             }
             return res.status(400).send('not found');
         });
