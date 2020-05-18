8c4a4adb1d49ac0ab05a4b73d3c80982823ee0d1
Meto <licovskim@gmail.com>
2020-05-18 14:54:37 +0200

    Merge branch 'master' of https://git.heroku.com/desolate-escarpment-53492

---
+++ client/build/precache-manifest.d06fcf589dfeb4f4072f551886aafd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967aedc37590863850066362ed0b84f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956b57eebd3a00f49940",
+    "url": "/static/css/main.47a3a25d.chunk.css"
+  },
+  {
+    "revision": "8517e1faffcbd03da501",
+    "url": "/static/js/2.d52d79b9.chunk.js"
+  },
+  {
+    "revision": "179592f3767e0670c0f89bc700f97ccc",
+    "url": "/static/js/2.d52d79b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "956b57eebd3a00f49940",
+    "url": "/static/js/main.313712eb.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
